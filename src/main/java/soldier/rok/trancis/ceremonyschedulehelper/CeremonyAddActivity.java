eb5fba01f0a6320f0dbf43807575bea827fb8e27
transpot32 <32831314+transpot32@users.noreply.github.com>
2017-10-17 14:45:04 +0900

    Hi

--- src/main/java/soldier/rok/trancis/ceremonyschedulehelper/CeremonyAddActivity.java
+++ src/main/java/soldier/rok/trancis/ceremonyschedulehelper/CeremonyAddActivity.java
@@ -1,59 +1,62 @@
 package soldier.rok.trancis.ceremonyschedulehelper;
 
+
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class CeremonyAddActivity extends AppCompatActivity {
     DatePicker mDate;
     TextView mTxtDate;
+    private String TAG = "PickerActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ceremony_add);
 
-        mDate = (DatePicker)findViewById(R.id.datepicker);
-        mTxtDate = (TextView)findViewById(R.id.txtdate);
-
-
-        //처음 DatePicker를 오늘 날짜로 초기화한다.
-        //그리고 리스너를 등록한다.
-        mDate.init(mDate.getYear(), mDate.getMonth(), mDate.getDayOfMonth(),
-                new DatePicker.OnDateChangedListener() {
-
-                    //값이 바뀔때마다 텍스트뷰의 값을 바꿔준다.
-                    @Override
-                    public void onDateChanged(DatePicker view, int year, int monthOfYear,
-                                              int dayOfMonth) {
-                        // TODO Auto-generated method stub
-                        //monthOfYear는 0값이 1월을 뜻하므로 1을 더해줌 나머지는 같다.
-                        mTxtDate.setText(String.format("%d/%d/%d", year,monthOfYear + 1
-                                , dayOfMonth));
-                    }
-
-                });
-
-
-        //선택기로부터 날짜 조사
-        findViewById(R.id.btnnow).setOnClickListener(new View.OnClickListener() {
-
-
-            //버튼 클릭시 DatePicker로부터 값을 읽어와서 Toast메시지로 보여준다.
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                String result = String.format("%d년 %d월 %d일", mDate.getYear(),
-                        mDate.getMonth() + 1, mDate.getDayOfMonth());
-
-            }
-
-        });
-
+        init();
     }
 
+    private void init() {
+
+        //Calendar를 이용하여 년, 월, 일, 시간, 분을 PICKER에 넣어준다.
+        final Calendar cal = Calendar.getInstance();
+
+        Log.e(TAG, cal.get(Calendar.YEAR) + "");
+        Log.e(TAG, cal.get(Calendar.MONTH) + 1 + "");
+        Log.e(TAG, cal.get(Calendar.DATE) + "");
+        Log.e(TAG, cal.get(Calendar.HOUR_OF_DAY) + "");
+        Log.e(TAG, cal.get(Calendar.MINUTE) + "");
+
+        //DATE PICKER DIALOG
+        findViewById(R.id.btn_date_picker_dialog).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                DatePickerDialog dialog = new DatePickerDialog(CeremonyAddActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int date) {
+
+                        String msg = String.format("%d 년 %d 월 %d 일", year, month + 1, date);
+                        Toast.makeText(CeremonyAddActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                }, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DATE));
+
+                dialog.getDatePicker().setMinDate(new Date().getTime());    //입력한 날짜 이후로 클릭 안되게 옵션
+                dialog.show();
+
+            }
+        });
+
+
+    }
 }
